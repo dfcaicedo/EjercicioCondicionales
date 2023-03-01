c7c0987377baa2abba276ebb455971216a23e711
Daniel Felipe Caicedo T <dfcaicedo@gmail.com>
2023-02-28 21:24:18 -0500

    Agregar archivos de proyecto.

---
+++ CondicionalesBasic/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Esto es Basic")
+        Dim a As Int32, b As Int32, c As Int32 ' Declaración de variables
+        a = Console.ReadLine() 'Recibir el valor del teclado
+        b = Console.ReadLine()
+        c = Console.ReadLine()
+        If a > b Then
+            'Este es el valor del si
+            If b > c Then
+                Console.WriteLine("a,b,c")
+            Else
+                'Este es el lado del no
+                If a > c Then
+                    Console.WriteLine("a,c,b")
+                Else
+                    Console.WriteLine("c,a,b")
+                End If
+            End If
+        Else
+            If a > c Then
+                Console.WriteLine("b,a,c")
+            Else
+                If b > c Then
+                    Console.WriteLine("b,c,a")
+                Else
+                    Console.WriteLine("c,b,a")
+                End If
+            End If
+        End If
+    End Sub
+
+End Module
---
+++ EjercicioCondicionales/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioCondicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto es c#");
+            int a, b, c; // declaración variables
+            a =Convert.ToInt32(Console.ReadLine()); //Recibir el valor del teclado
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                //este lado es el valor del si
+                if (b > c)
+                {
+                    Console.WriteLine("a,b,c");
+                }
+                else
+                {
+                    //Este es el lado del no
+                    if (a > c)
+                    {
+                        Console.WriteLine("a,c,b");
+                    }
+                    else
+                    {
+                        Console.WriteLine("c,a,b");
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
